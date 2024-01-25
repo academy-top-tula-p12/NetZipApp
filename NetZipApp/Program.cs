92faf66f9735345894499840bbfe9c6762a06728
maxefimov <efimov.top.academy@gmail.com>
2024-01-25 20:53:32 +0300

    Добавьте файлы проекта.

---
+++ NetZipApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Compression;
+
+string srcDir = @"olddir";
+string dstDir = @"newdir";
+string fileZip = @"archive.zip";
+
+//ZipFile.CreateFromDirectory(srcDir, fileZip);
+//Console.WriteLine("Directory add to archive");
+//ZipFile.ExtractToDirectory(fileZip, dstDir);
+//Console.WriteLine("New directory from archive");
+
+using(FileStream stream = File.Open(fileZip, FileMode.Open))
+{
+    using(ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Update))
+    {
+        ZipArchiveEntry entry = archive.CreateEntry("myfile.txt");
+        using(StreamWriter writer = new StreamWriter(entry.Open()))
+        {
+            writer.WriteLine("Hello world");
+        }
+    }
+}
